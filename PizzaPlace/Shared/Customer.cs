8245b396bd8dd8c83f9953e5db729cc8c2003655
Dirk <dirkos@hotmail.de>
2020-06-29 14:36:48 +0200

    Validation

--- PizzaPlace/Shared/Customer.cs
+++ PizzaPlace/Shared/Customer.cs
@@ -1,9 +1,12 @@
-﻿// using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 namespace PizzaPlace.Shared
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50)]
         public string Name { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
